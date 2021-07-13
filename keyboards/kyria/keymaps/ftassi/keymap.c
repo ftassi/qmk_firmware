89c8464a22a5725a6ea018be15fa0ffe26e32147
Francesco Tassi <tassi.francesco@gmail.com>
2021-07-13 23:20:44 +0200

    Improve layer keys on thumb cluster

--- keyboards/kyria/keymaps/ftassi/keymap.c
+++ keyboards/kyria/keymaps/ftassi/keymap.c
@@ -75,19 +75,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |RAIS/ESC|   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/BS |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
+ * |Ctrl/BS |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |' "/I3wm|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|      |  |      |LShift|   N  |   M  | ,  < | . >  | /  ? |  - _   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        | Nav  | Del  | GUI  | Esc  | Enter|  | Bksp | Space| Tab  | Bksp | AltGr|
- *                        |      |      |      | Num  | Symb |  | Symb | Num  | Alt  |      |      |
+ *                        |      |      | Numb | Alt  |      |  |      |      | Symb |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
       LT(_NUMBERS, KC_ESC),       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
       MT(MOD_LCTL, KC_BSPC),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(_I3WM, KC_QUOT),
       KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT, XXXXXXX, XXXXXXX, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-              MO(_NAV), KC_DEL, KC_LGUI, LT(_NUMBERS, KC_ESC), LT(_SYMBOL, KC_ENT), LT(_SYMBOL, KC_BSPC), LT(_NUMBERS, KC_SPC), MT(MOD_LALT, KC_TAB),  KC_BSPC, KC_RALT
+              MO(_NAV), KC_DEL, LT(_NUMBERS, KC_LGUI), MT(MOD_LALT, KC_ESC), KC_ENT, KC_BSPC, KC_SPC, LT(_SYMBOL, KC_TAB),  KC_BSPC, KC_RALT
     ),
 /*
  * Lower Layer: Symbols
