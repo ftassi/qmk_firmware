467f0722562a3a26e8cc7cebb0faae112afe7c29
Francesco Tassi <tassi.francesco@gmail.com>
2021-07-13 23:27:09 +0200

    Add an extra right control

--- keyboards/kyria/keymaps/ftassi/keymap.c
+++ keyboards/kyria/keymaps/ftassi/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|      |  |      |LShift|   N  |   M  | ,  < | . >  | /  ? |  - _   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | Nav  | Del  | GUI  | Esc  | Enter|  | Bksp | Space| Tab  | Bksp | AltGr|
+ *                        | Nav  | Del  | GUI  | Esc  | Enter|  | Bksp | Space| Tab  | AltGr| CTRL |
  *                        |      |      | Numb | Alt  |      |  |      |      | Symb |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       LT(_NUMBERS, KC_ESC),       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
       MT(MOD_LCTL, KC_BSPC),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(_I3WM, KC_QUOT),
       KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT, XXXXXXX, XXXXXXX, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-              MO(_NAV), KC_DEL, LT(_NUMBERS, KC_LGUI), MT(MOD_LALT, KC_ESC), KC_ENT, KC_BSPC, KC_SPC, LT(_SYMBOL, KC_TAB),  KC_BSPC, KC_RALT
+              MO(_NAV), KC_DEL, LT(_NUMBERS, KC_LGUI), MT(MOD_LALT, KC_ESC), KC_ENT, KC_BSPC, KC_SPC, LT(_SYMBOL, KC_TAB), KC_RALT, MOD_RCTL
     ),
 /*
  * Lower Layer: Symbols
