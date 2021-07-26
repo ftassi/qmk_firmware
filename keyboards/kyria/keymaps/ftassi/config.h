f7b94b6df0f893008305122cb2ea05c2f899648a
Francesco Tassi <tassi.francesco@gmail.com>
2021-07-26 14:49:35 +0200

    Improva right modifiers usability with ignore mod tap interrupt

--- keyboards/kyria/keymaps/ftassi/config.h
+++ keyboards/kyria/keymaps/ftassi/config.h
@@ -28,6 +28,7 @@
   #define RGBLIGHT_LIMIT_VAL 150
 #endif
 
+#define IGNORE_MOD_TAP_INTERRUPT
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 // #define SPLIT_USB_DETECT
 // #define NO_USB_STARTUP_CHECK
